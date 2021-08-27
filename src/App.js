a584f7a4d04d73ccc2198ba6b75607e8cbcb83f3
Khushwant Parmar <ksparmar@sfu.ca>
2021-08-27 01:11:05 -0700

    added base structure for accordion-no functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import React from 'react';
+import Accordion  from './components/Accordion';
+
+//static array used for content, we are hardcoding it
+const items = [
+    {
+        title: 'What is React?',
+        content:'React is a front end javascript framework'
+    },
+    {
+        title: 'Why use React?',
+        content: 'React is favorite JS library among engineers'
+
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use it by making components'
+    }
+];
 
 export default ()=>{
-    return <h1> Widgets App </h1>
+    return (
+        <div>
+            <Accordion items = {items}/>
+        </div>
+    )
 };
